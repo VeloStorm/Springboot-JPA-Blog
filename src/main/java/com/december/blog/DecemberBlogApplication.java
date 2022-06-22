270465422e3d1fe7c2174cf5af64d8206e7c062b
pd0a7812 <pd0a7812@posco.com>
2022-06-22 14:59:40 +0900

    Springboot 환경세팅 완료

---
+++ src/main/java/com/december/blog/DecemberBlogApplication.java
@@ -0,0 +1,13 @@
+package com.december.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecemberBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecemberBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/december/blog/DecemberBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.december.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecemberBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
